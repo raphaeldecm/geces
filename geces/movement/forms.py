7c1168c9fe0549a913161150f9c1abd0e194a70d
Raphael Muniz <raphael@copin.ufcg.edu.br>
2023-10-23 21:42:23 -0300

    include mask money

--- geces/movement/forms.py
+++ geces/movement/forms.py
@@ -1,9 +1,22 @@
+from decimal import Decimal
+
 from django import forms
 
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+
+    price = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'money'}
+        )
+    )
+
     class Meta:
         model = Product
         fields = ('name', 'price', 'stock', 'image')
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        return Decimal(price.replace(',', '.'))
