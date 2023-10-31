1ea42948507412ae492a07eaf4f48c8ab9b9e469
Raphael Muniz <raphael@copin.ufcg.edu.br>
2023-10-31 10:57:24 -0300

    include is active user form

--- geces/users/forms.py
+++ geces/users/forms.py
@@ -34,7 +34,7 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
 
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
-        fields = ("email", "name", "group")
+        fields = ("email", "name", "group", "is_active")
         field_classes = {"email": EmailField}
         error_messages = {
             "email": {"unique": _("This email has already been taken.")},
