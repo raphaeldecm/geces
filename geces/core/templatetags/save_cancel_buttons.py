6ada5120c8dae956de3797ccd040a04198e0729c
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2023-11-06 16:40:38 -0300

    update template tag

--- geces/core/templatetags/save_cancel_buttons.py
+++ geces/core/templatetags/save_cancel_buttons.py
@@ -15,13 +15,12 @@ delete -- true ou false para indicar que há um botāo de voltar
 @register.inclusion_tag('templatetags/save_cancel_buttons.html', takes_context=True)
 def save_cancel_buttons(context, *args, **kwargs):
 
-    link_back = kwargs.get("link_back")
+    link_back = kwargs.get("link_back", None)  # Pode ser None se 'link_back' não estiver definido nos kwargs
+    link_forward = kwargs.get("link_forward", None)  # Pode ser None se 'link_forward' não estiver definido nos kwargs
+    has_form = kwargs.get("has_form", False)  # Por padrão, assumimos que não é uma página com formulário
 
-    if "url_back" in kwargs:
-        k = {}
-        link_back = reverse_lazy(kwargs.get("url_back"), kwargs=k)
-
-    print(link_back)
     return {
         "link_back": link_back,
+        "link_forward": link_forward,
+        "has_form": has_form,
     }
