96b622bc1a059f3a113de3e46a7623e287e795ca
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2023-11-16 18:14:58 -0300

    Add form fields and update infobox colors in people templates

--- geces/people/forms.py
+++ geces/people/forms.py
@@ -8,12 +8,79 @@ class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
         fields = ["city", "address", "zip_code", "phone"]
+    
+    city = forms.CharField(
+        label='Cidade',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    address = forms.CharField(
+        label='Endereço',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    zip_code = forms.CharField(
+        label='CEP',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    phone = forms.CharField(
+        label='Telefone',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
 
 
 class ResponsibleForm(forms.ModelForm):
     class Meta:
         model = Responsible
         fields = ["id", "name", "gender", "email"]
+
+    name = forms.CharField(
+        label='Nome',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        label='E-mail',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+    phone_number = forms.CharField(
+        label='Telefone',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+    gender = forms.ChoiceField(
+        choices=Responsible.Gender.choices,
+        label="Gênero",
+        widget=forms.Select(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
 
 
 class StudentForm(forms.ModelForm):
